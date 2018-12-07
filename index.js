90f9a9659a51bef7f5aaf26992960f592b3ff7c3
Christopher Thorn <chris.thorn@lytics.com>
2018-12-07 09:24:00 -0800

    update for lytics

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const GitHubApi = require('github')
+const GitHubApi = require("github");
 
 const config = {
-  ciUser: "githubUser", // document your CI user here
-  ciAccessToken: "personalAccessToken", // document which token you're using
-  repoOwner: "beatport"
-}
+  ciUser: "[ci machine user account]",
+  ciAccessToken: "[personal access token]",
+  repoOwner: "[github org]"
+};
 
 // Can be one of error, failure, pending, or success.
 const statusMap = {
@@ -15,12 +15,12 @@ const statusMap = {
   CANCELLED: "failure",
   TIMEOUT: "error",
   INTERNAL_ERROR: "error"
-}
+};
 
 // setCIStatus is the main function.
-module.exports.setCIStatus = (event) => {
-  build = eventToBuild(event.data.data)
-  console.log(`gcloud container builds describe --format=json ${build.id}`)
+module.exports.setCIStatus = event => {
+  build = eventToBuild(event.data.data);
+  console.log(`gcloud container builds describe --format=json ${build.id}`);
 
   const {
     id,
@@ -28,51 +28,56 @@ module.exports.setCIStatus = (event) => {
     status,
     steps,
     images,
-    sourceProvenance: {
-      resolvedRepoSource: repoSource
-    },
+    sourceProvenance: { resolvedRepoSource: repoSource },
     logUrl,
     tags,
     createTime,
-    finishTime,
-  } = build
+    finishTime
+  } = build;
 
-  const ghStatus = statusMap[status]
+  const ghStatus = statusMap[status];
 
-  if (!repoSource || !ghStatus) return
+  console.log("github status:", ghStatus);
+  console.log("repo source:", repoSource);
 
-  const ghRepo = repoSource.repoName.indexOf(`github-${config.repoOwner}-`) == 0
-    ? repoSource.repoName.replace(`github-${config.repoOwner}-`, '')
-    : false
-  if (!ghRepo) return
+  if (!repoSource || !ghStatus) return;
 
-  const prettyTags = tags && tags.filter(t => !t.match(/(event|trigger|eval|invocation)-[\w-]{36}/))
-  const ghContext = prettyTags && prettyTags.length > 0
-    ? `${projectId}/gcb: ${prettyTags.join('/')}`
-    : `${projectId}/gcb: ${id.substring(0,8)}`
+  const ghRepo =
+    repoSource.repoName.indexOf(`github_${config.repoOwner}_`) == 0
+      ? repoSource.repoName.replace(`github_${config.repoOwner}_`, "")
+      : false;
 
-  const lastStep = steps.filter( s => s.timing && s.timing.startTime ).pop()
-  const failureDescription = (ghStatus=='failure' || ghStatus=='error')
-    ? ' · ' + (lastStep ? `${lastStep.id} `:'') + status.toLowerCase()
-    : ''
-  const ghDescription = (
-    createTime && finishTime
-    ? secondsToString((new Date(finishTime) - new Date(createTime)) / 1000) + failureDescription
+  console.log("github repo:", ghRepo);
+
+  if (!ghRepo) return;
+
+  const prettyTags =
+    tags &&
+    tags.filter(t => !t.match(/(event|trigger|eval|invocation)-[\w-]{36}/));
+  const ghContext =
+    prettyTags && prettyTags.length > 0
+      ? `${projectId}/gcb: ${prettyTags.join("/")}`
+      : `${projectId}/gcb: ${id.substring(0, 8)}`;
+
+  const lastStep = steps.filter(s => s.timing && s.timing.startTime).pop();
+
+  const failureDescription =
+    ghStatus == "failure" || ghStatus == "error"
+      ? " · " + (lastStep ? `${lastStep.name} ` : "") + status.toLowerCase()
+      : "";
+  const ghDescription = (createTime && finishTime
+    ? secondsToString((new Date(finishTime) - new Date(createTime)) / 1000) +
+      failureDescription
     : images && images.length > 0
-      ? `${images.join('\n')}`
-      : ''
-  ).substring(0,140)
+      ? `${images.join("\n")}`
+      : ""
+  ).substring(0, 140);
 
-  console.log(status, ghStatus)
-  console.log(ghRepo, repoSource)
-  console.log(ghContext, tags)
-  console.log(ghDescription, createTime, finishTime, images)
-
-  let github = new GitHubApi()
+  let github = new GitHubApi();
   github.authenticate({
-    type: 'token',
+    type: "token",
     token: config.ciAccessToken
-  })
+  });
 
   let request = {
     owner: config.repoOwner,
@@ -82,25 +87,26 @@ module.exports.setCIStatus = (event) => {
     target_url: logUrl,
     description: ghDescription,
     context: ghContext
-  }
+  };
 
-  console.log(JSON.stringify(request, null, 2))
+  console.log(JSON.stringify(request, null, 2));
 
-  return github.repos.createStatus(request)
-}
+  return github.repos.createStatus(request);
+};
 
 // eventToBuild transforms pubsub event message to a build object.
-const eventToBuild = (data) =>
-  JSON.parse(new Buffer(data, 'base64').toString())
+const eventToBuild = data => JSON.parse(new Buffer(data, "base64").toString());
 
 // secondsToString turns a number of seconds into a human-readable duration.
-const secondsToString = (s) => {
-  const years   = Math.floor(s / 31536000)
-  const days    = Math.floor((s % 31536000) / 86400)
-  const hours   = Math.floor(((s % 31536000) % 86400) / 3600)
-  const minutes = Math.floor((((s % 31536000) % 86400) % 3600) / 60)
-  const seconds = Math.floor((((s % 31536000) % 86400) % 3600) % 60)
+const secondsToString = s => {
+  const years = Math.floor(s / 31536000);
+  const days = Math.floor((s % 31536000) / 86400);
+  const hours = Math.floor(((s % 31536000) % 86400) / 3600);
+  const minutes = Math.floor((((s % 31536000) % 86400) % 3600) / 60);
+  const seconds = Math.floor((((s % 31536000) % 86400) % 3600) % 60);
 
-  return `${years}y ${days}d ${hours}h ${minutes}m ${seconds}s`
-    .replace(/^(0[ydhm] )*/g, '')
-}
+  return `${years}y ${days}d ${hours}h ${minutes}m ${seconds}s`.replace(
+    /^(0[ydhm] )*/g,
+    ""
+  );
+};
